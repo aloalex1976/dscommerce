fc4720f9afe41865f557c4a63eca9ab86d281d6b
Alex Batista Dias <aloalex@gmail.com>
2025-07-18 12:00:55 -0300

    Payment

--- src/main/java/com/treinamento/dscommerce/entities/Payment.java
+++ src/main/java/com/treinamento/dscommerce/entities/Payment.java
@@ -2,47 +2,40 @@ package com.treinamento.dscommerce.entities;
 
 import java.time.Instant;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_order")
-public class Order {
-
+@Table(name = "tb_payment")
+public class Payment {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant moment;
-	private OrderStatus status;
 	
-	@ManyToOne
-	@JoinColumn(name = "client_id")
-	private User client;
-	
-	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-	private Payment payment;
+	@OneToOne
+	@MapsId
+	private Order order;
 
-	public Order() {
+	public Payment() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public Order(Long id, Instant moment, OrderStatus status, User client) {
+	public Payment(Long id, Instant moment, Order order) {
 		super();
 		this.id = id;
 		this.moment = moment;
-		this.status = status;
-		this.client = client;
+		this.order = order;
 	}
 
 	public Long getId() {
@@ -61,22 +54,14 @@ public class Order {
 		this.moment = moment;
 	}
 
-	public OrderStatus getStatus() {
-		return status;
+	public Order getOrder() {
+		return order;
 	}
 
-	public void setStatus(OrderStatus status) {
-		this.status = status;
-	}
-
-	public User getClient() {
-		return client;
-	}
-
-	public void setClient(User client) {
-		this.client = client;
+	public void setOrder(Order order) {
+		this.order = order;
 	}
 	
 	
-	
+
 }
