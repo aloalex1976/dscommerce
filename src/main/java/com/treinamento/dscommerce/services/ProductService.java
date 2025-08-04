2aecd1db57b00cf50ecdda5034e9fcbc2e8cc1d0
Alex Batista Dias <aloalex@gmail.com>
2025-08-04 10:27:07 -0300

    GET Products PAGINADOS

--- src/main/java/com/treinamento/dscommerce/services/ProductService.java
+++ src/main/java/com/treinamento/dscommerce/services/ProductService.java
@@ -3,6 +3,8 @@ package com.treinamento.dscommerce.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,7 +27,11 @@ public class ProductService {
 		return dto;
 	}
 	
-	
-	 
-
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		
+		Page<Product> result = repository.findAll(pageable);
+		
+		return result.map(x -> new ProductDTO(x));
+	}
 }
